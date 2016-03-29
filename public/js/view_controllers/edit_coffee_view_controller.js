fd104b3387fc6cc98120a9e010c29efd073fa27b
John Hodmon <johnhodmon@gmail.com>
2016-03-29 18:47:03 +0100

    update feature working fully

--- public/js/view_controllers/edit_coffee_view_controller.js
+++ public/js/view_controllers/edit_coffee_view_controller.js
@@ -3,24 +3,29 @@ var app = angular.module('CoffeeMate');
 app.controller('edit_coffee_view_controller', ['$scope', '$location','$http','$routeParams', function($scope,$location,$http,$routeParams) {
 
     $scope.edit_coffee_form = {};
-    var id=$routeParams.id;
-    var coffee={};
-    $http.get('/coffees/'+id)
-        .success(function(data){
-            console.log("data: "+data);
-            coffee=data;
+    getCoffeeToEdit();
 
-        })
-        .error(function(data)
-        {
-            console.log('Error'+data)
-        });
 
-    $scope.edit_coffee_form.name=coffee.name;
-    $scope.edit_coffee_form.coffee_shop=coffee.coffee_shop;
-    $scope.edit_coffee_form.price=coffee.coffee_shop;
-    $scope.edit_coffee_form.stars=coffee.stars;
-    $scope.edit_coffee_form.favourite=coffee.favourite;
+
+    function getCoffeeToEdit() {
+        $http.get('/coffees/'+$routeParams.id)
+            .success(function (data) {
+                console.log("data: " + data);
+                var coffee = data[0];
+
+                $scope.edit_coffee_form.name=coffee.name;
+                $scope.edit_coffee_form.coffee_shop=coffee.coffee_shop;
+                $scope.edit_coffee_form.price=coffee.price;
+                $scope.edit_coffee_form.stars=coffee.stars;
+                $scope.edit_coffee_form.favourite=coffee.favourite;
+
+            })
+            .error(function (data) {
+                console.log('Error' + data)
+            });
+    }
+
+
 
 
     $scope.editCoffee=function(coffee)
@@ -39,7 +44,7 @@ app.controller('edit_coffee_view_controller', ['$scope', '$location','$http','$r
 
         };
 
-        $http.put('/coffees/'+id,updatedCoffee)
+        $http.put('/coffees/'+$routeParams.id,updatedCoffee)
             .success(function(data){
                 console.log(data);
                 $location.path('/coffees');
